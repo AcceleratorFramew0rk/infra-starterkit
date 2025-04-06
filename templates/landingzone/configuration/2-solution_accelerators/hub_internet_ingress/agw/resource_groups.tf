02d62f74d1b8cd464fc66e7274f06359cd0f8337
thiamsoontan <thiamsoontan@microsoft.com>
2025-04-06 21:48:02 +0800

    rename resource group

--- templates/landingzone/configuration/2-solution_accelerators/hub_internet_ingress/agw/resource_groups.tf
+++ templates/landingzone/configuration/2-solution_accelerators/hub_internet_ingress/agw/resource_groups.tf
@@ -1,7 +1,7 @@
 resource "azurerm_resource_group" "this" {
   count = try(local.global_settings.resource_group_name, null) == null ? 1 : 0
   
-  name     = "${module.naming.resource_group.name}-solution-accelerators-agwavm-internet" 
+  name     = "${module.naming.resource_group.name}-solution-accelerators-agw-internet" 
   location = "${local.global_settings.location}" 
 }
 
