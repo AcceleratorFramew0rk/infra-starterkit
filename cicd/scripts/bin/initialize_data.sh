d723bf50c3738ae458cb94c7315ec6def981b8ba
thiamsoontan <thiamsoontan@microsoft.com>
2025-04-28 14:39:13 +0800

    Update initialize_data.sh

--- cicd/scripts/bin/initialize_data.sh
+++ cicd/scripts/bin/initialize_data.sh
@@ -12,11 +12,11 @@ SOLUTION_ACCELERATOR=$3
 
 
 # get config.yaml from input and write to file
-WORKING_DIRECTORY=$(pwd)
+WORKING_DIRECTORY=$(pwd) # pwd should always be ./starterkit or $(pwd)/starterkit
 echo $WORKING_DIRECTORY
 decoded_config_yaml=$(echo "${CONFIG_YAML}" | base64 --decode)
-echo "$decoded_config_yaml" > "${WORKING_DIRECTORY}/${STARTERKIT_FOLDER}/templates/landingzone/configuration/0-launchpad/scripts/config.yaml"
-echo "$decoded_config_yaml" > "${WORKING_DIRECTORY}/config.yaml"
+echo "$decoded_config_yaml" > "./templates/landingzone/configuration/0-launchpad/scripts/config.yaml"
+echo "$decoded_config_yaml" > "./config.yaml"
 
 # ** IMPORTANT: ensure config.yaml file is available as the path is hardcoded in the terraform code
 
@@ -37,11 +37,11 @@ selectedServicesConfig=$(echo "$decoded_solution_accelerator" | jq -r '.[1].sele
 echo "selectedServicesConfig:"
 echo $selectedServicesConfig
 # write to files
-echo "$selectedServices" > "${WORKING_DIRECTORY}/${STARTERKIT_FOLDER}/cicd/scripts/config/selectedServices.json"
-echo "$selectedServicesConfig" > "${WORKING_DIRECTORY}/${STARTERKIT_FOLDER}/cicd/scripts/config/selectedServicesConfig.json"
+echo "$selectedServices" > "./cicd/scripts/config/selectedServices.json"
+echo "$selectedServicesConfig" > "./cicd/scripts/config/selectedServicesConfig.json"
 
 # generate config.yaml
-sudo chmod -R -f 777 "${WORKING_DIRECTORY}/starterkit/cicd/scripts/"
+sudo chmod -R -f 777 "./cicd/scripts/"
 
 # Read YAML file and extract "prefix"
 PREFIX=$(yq -r '.prefix' './config.yaml')
@@ -58,9 +58,9 @@ echo $LOCATION
 echo $VNET_PROJECT_NAME
 echo $VNET_DEVOPS_NAME
 
-cd ./starterkit
-"${WORKING_DIRECTORY}/${STARTERKIT_FOLDER}/cicd/scripts/bin/generate_config.sh" $PREFIX $SUBSCRIPTION_ID $ENVIRONMENT $LOCATION $VNET_PROJECT_NAME $VNET_DEVOPS_NAME
+
+./cicd/scripts/bin/generate_config.sh $PREFIX $SUBSCRIPTION_ID $ENVIRONMENT $LOCATION $VNET_PROJECT_NAME $VNET_DEVOPS_NAME
 
 # # copy final config.yaml to destination
-cp "${WORKING_DIRECTORY}/${STARTERKIT_FOLDER}/cicd/scripts/config/output_config.yaml" "${WORKING_DIRECTORY}/config.yaml"
-cp "${WORKING_DIRECTORY}/${STARTERKIT_FOLDER}/cicd/scripts/config/output_config.yaml" "${WORKING_DIRECTORY}/${STARTERKIT_FOLDER}/templates/landingzone/configuration/0-launchpad/scripts/config.yaml"
+cp "./cicd/scripts/config/output_config.yaml" "./../config.yaml"
+cp "./cicd/scripts/config/output_config.yaml" "./templates/landingzone/configuration/0-launchpad/scripts/config.yaml"
