48d2a639274c047d0d71aef45694a4a281e11a41
thiamsoontan <thiamsoontan@microsoft.com>
2025-04-29 15:20:49 +0800

    Update resource_groups.tf

--- templates/landingzone/configuration/2-solution_accelerators/project/container_instance/resource_groups.tf
+++ templates/landingzone/configuration/2-solution_accelerators/project/container_instance/resource_groups.tf
@@ -1,7 +1,7 @@
 resource "azurerm_resource_group" "this" {
   count = try(local.global_settings.resource_group_name, null) == null ? 1 : 0
   
-  name     = "${module.naming.resource_group.name}-solution-accelerators-containergroupavm" 
+  name     = "${module.naming.resource_group.name}-solution-accelerators-containergroup" 
   location = "${try(local.global_settings.location, var.location)}" 
 }
 
