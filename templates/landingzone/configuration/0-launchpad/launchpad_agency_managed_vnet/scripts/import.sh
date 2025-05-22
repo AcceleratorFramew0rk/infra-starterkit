e5cb4ee88dde761640d05832cd56ff576c6d54eb
thiamsoontan <thiamsoontan@microsoft.com>
2025-05-22 17:48:37 +0800

    Update import.sh

--- templates/landingzone/configuration/0-launchpad/launchpad_agency_managed_vnet/scripts/import.sh
+++ templates/landingzone/configuration/0-launchpad/launchpad_agency_managed_vnet/scripts/import.sh
@@ -8,9 +8,6 @@ WORKING_DIR="${1:-/tf/avm}"
 echo "Working Directory: ${WORKING_DIR}"
 
 echo "getting the prefix from config.yaml"
-# cannot change mode in github pipeline
-# sudo chmod -R -f 777 "${WORKING_DIR}/templates/landingzone/configuration"
-
 
 #------------------------------------------------------------------------
 # create launchpad storage account and containers
@@ -21,11 +18,71 @@ CONFIG_FILE="${WORKING_DIR}/templates/landingzone/configuration/0-launchpad/scri
 echo "Config file: ${CONFIG_FILE}"
 
 PREFIX=$(yq  -r '.prefix' "${CONFIG_FILE}")
-echo "en getting the prefix from config.yaml"
+echo "getting the prefix from config.yaml"
 # cd /tf/avm/templates/landingzone/configuration/0-launchpad/launchpad_healthcare
 cd "${WORKING_DIR}/templates/landingzone/configuration/0-launchpad/launchpad_agency_managed_vnet"
 pwd
-./scripts/launchpad.sh $PREFIX
+# ./scripts/launchpad.sh $PREFIX
+
+#------------------------------------------------------------------------
+# launchpad logic
+#------------------------------------------------------------------------
+
+# Generate storage acc name to store state file
+LOC="southeastasia"
+PROJECT_CODE="${PREFIX}"
+RND_NUM=$(env LC_CTYPE=C tr -dc 'a-z' </dev/urandom | fold -w 3 | head -n 1)
+
+RG_NAME="${PROJECT_CODE}-rg-launchpad"
+STG_NAME="${PROJECT_CODE}stgtfstate${RND_NUM}"
+STG_NAME="${STG_NAME//-/}"
+CONTAINER1="0-launchpad"
+CONTAINER2="1-landingzones"
+CONTAINER3="2-solution-accelerators"
+
+# Check if the resource group already exists
+az group show --name $RG_NAME > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+    echo -p "ERROR: Resource group $RG_NAME already exists. Exiting."
+    exit 1
+else
+    # If the resource group does not exist, attempt to create it
+    az group create --name $RG_NAME --location $LOC
+    if [ $? -eq 0 ]; then
+        echo "Resource group $RG_NAME created successfully."
+    else
+        echo -p "ERROR: Failed to create resource group $RG_NAME. Exiting."
+        exit 1
+    fi
+fi
+
+# Create Storage account and containers for storing state files
+az storage account create --name $STG_NAME --resource-group $RG_NAME --location $LOC --sku Standard_LRS --kind StorageV2 --allow-blob-public-access true --min-tls-version TLS1_2
+if [ $? -eq 0 ]; then
+    echo -p "ERROR: Failed to create storage. Exiting."
+    exit 1
+fi
+az storage container create --account-name $STG_NAME --name $CONTAINER1 --public-access blob --fail-on-exist
+if [ $? -eq 0 ]; then
+    echo -p "ERROR: Failed to create container $CONTAINER1. Exiting."
+    exit 1
+fi
+az storage container create --account-name $STG_NAME --name $CONTAINER2 --public-access blob --fail-on-exist
+if [ $? -eq 0 ]; then
+    echo -p "ERROR: Failed to create container $CONTAINER2. Exiting."
+    exit 1
+fi
+az storage container create --account-name $STG_NAME --name $CONTAINER3 --public-access blob --fail-on-exist
+if [ $? -eq 0 ]; then
+    echo -p "ERROR: Failed to create container $CONTAINER3. Exiting."
+    exit 1
+fi
+
+
+#------------------------------------------------------------------------
+# end launchpad logic
+#------------------------------------------------------------------------
 
 
 #------------------------------------------------------------------------
