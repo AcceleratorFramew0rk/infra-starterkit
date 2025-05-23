73fc2aa0875c970c2d268c436ba06ba03bf119b1
thiamsoontan <thiamsoontan@microsoft.com>
2025-05-23 12:18:57 +0800

    Update main.tf

--- templates/landingzone/configuration/2-solution_accelerators/project/aks_avm_ptn/main.tf
+++ templates/landingzone/configuration/2-solution_accelerators/project/aks_avm_ptn/main.tf
@@ -98,7 +98,7 @@ module "aks_cluster" {
     service_cidr        = "172.16.0.0/18" #"10.0.0.0/16"
   }
 
-  node_resource_group = try(local.global_settings.resource_group_name, null) == null ? "${module.naming.resource_group.name}-solution-accelerators-aks-nodes" : "${azurerm_resource_group.this.0.name}-aks-nodes"
+  node_resource_group = try(local.global_settings.resource_group_name, null) != null ? "${module.naming.resource_group.name}-aks-nodes" : "${azurerm_resource_group.this.0.name}-aks-nodes"
   container_registry_id = module.container_registry.resource.id 
 
   log_analytics_workspace_id = try(local.remote.log_analytics_workspace.id, null) != null ? local.remote.log_analytics_workspace.id : var.log_analytics_workspace_id
