61f447c9b3d9e15179847759bbcba4580f89780f
thiamsoontan <thiamsoontan@microsoft.com>
2025-05-23 11:30:21 +0800

    add acr to aks ptn

--- templates/landingzone/configuration/2-solution_accelerators/project/aks_avm_ptn/locals.tf
+++ templates/landingzone/configuration/2-solution_accelerators/project/aks_avm_ptn/locals.tf
@@ -1,17 +1,18 @@
-# read current level terraform state - appservice
-data "terraform_remote_state" "container_registry" {
-  backend = "azurerm"
+# # read current level terraform state - appservice
+# data "terraform_remote_state" "container_registry" {
+#   backend = "azurerm"
 
-  config = {
-    resource_group_name  = var.resource_group_name
-    storage_account_name = var.storage_account_name
-    container_name       = "2-solution-accelerators"
-    key                  = "solution-accelerators-project-acr.tfstate" 
-  }
-}
+#   config = {
+#     resource_group_name  = var.resource_group_name
+#     storage_account_name = var.storage_account_name
+#     container_name       = "2-solution-accelerators"
+#     key                  = "solution-accelerators-project-acr.tfstate" 
+#   }
+# }
 
 
-locals {
-  container_registry = try(data.terraform_remote_state.container_registry.outputs.resource, null)     
-}
+# locals {
+#   container_registry = try(data.terraform_remote_state.container_registry.outputs.resource, null)     
+# }
 
+# Note: change to using local container registry.
