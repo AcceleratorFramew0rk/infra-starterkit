088bb303aecfd0729b62c6265cac34c71ac7eda8
thiamsoontan <thiamsoontan@microsoft.com>
2025-05-27 13:51:57 +0800

    Update resource_groups.tf

--- templates/landingzone/configuration/2-solution_accelerators/project/vm_db/resource_groups.tf
+++ templates/landingzone/configuration/2-solution_accelerators/project/vm_db/resource_groups.tf
@@ -1,7 +1,7 @@
 resource "azurerm_resource_group" "this" {
   count = try(local.global_settings.resource_group_name, null) == null ? 1 : 0
   
-  name = lower("${module.naming.resource_group.name}-solution-accelerators-${var.vnet_type}virtualmachine-${var.virtualmachine_os_type}")
+  name = lower("${module.naming.resource_group.name}-solution-accelerators-${var.vnet_type}virtualmachinedb-${var.virtualmachine_os_type}")
   location = "${try(local.global_settings.location, var.location)}" 
 }
 
