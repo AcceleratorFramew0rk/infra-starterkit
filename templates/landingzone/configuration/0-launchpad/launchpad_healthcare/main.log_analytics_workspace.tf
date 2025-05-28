c2e95bc5153db01fe33e18496bf296dd3d3bb27b
thiamsoontan <thiamsoontan@microsoft.com>
2025-05-28 15:36:10 +0800

    clean up unused folder

--- templates/landingzone/configuration/0-launchpad/launchpad_healthcare/main.log_analytics_workspace.tf
+++
@@ -1,9 +0,0 @@
-
-# resource "azurerm_log_analytics_workspace" "gcci_agency_workspace" {
-#   count = try(var.gcci_agency_law_enabled, false) == true ? 1 : 0
-
-#   name = try(local.log_analytics_workspace_name, null) != null ? local.log_analytics_workspace_name : "gcci-agency-workspace"
-#   location = var.location
-#   resource_group_name = "gcci-agency-law"
-# }
-
--- templates/landingzone/configuration/0-launchpad/launchpad_healthcare_law/README.md
+++
@@ -1,2 +0,0 @@
-# Terraform code with azurerm_log_analytics_workspace
-# to import log analytics workspace into gcci-platform.tfstate
